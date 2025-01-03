62f41db9f2348d664bf1df551d54199fb4d560ae
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-01-03 14:43:40 +0900

    initial commit

---
+++ src/main/java/madcamp24w/friends/FriendsApplication.java
@@ -0,0 +1,13 @@
+package madcamp24w.friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsApplication.class, args);
+	}
+
+}
