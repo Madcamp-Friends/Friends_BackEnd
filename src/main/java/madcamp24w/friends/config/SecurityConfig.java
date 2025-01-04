21d601f928b1dc0c8f26942c31d5a628fd104650
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-01-04 23:56:47 +0900

    feat : cors 에러 해결

--- src/main/java/madcamp24w/friends/config/SecurityConfig.java
+++ src/main/java/madcamp24w/friends/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                     cors.configurationSource(request -> {
                         CorsConfiguration config = new CorsConfiguration();
                         config.setAllowCredentials(true);
+                        config.addAllowedOrigin("http://localhost:3000");
                         config.addAllowedHeader("*");
                         config.addAllowedMethod("*");
                         return config;
