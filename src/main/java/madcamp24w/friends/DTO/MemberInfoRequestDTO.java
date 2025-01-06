ce1a95f7963e0f0c64f5826f92da43a5f03a66b1
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-01-07 01:31:46 +0900

    request, response dto 추가

--- src/main/java/madcamp24w/friends/DTO/MemberInfoRequestDTO.java
+++ src/main/java/madcamp24w/friends/DTO/MemberInfoRequestDTO.java
@@ -2,7 +2,7 @@ package madcamp24w.friends.DTO;
 
 public class MemberInfoRequestDTO {
     private String nickname;
-    private String password;
+    //private String password;
     private String email;
 
 }
